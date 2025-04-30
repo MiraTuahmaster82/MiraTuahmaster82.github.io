bb42d462431ec349d4b18ce98fd6662901f14a7c
SquidR <squidplays1337@gmail.com>
2025-04-30 16:35:40 -0500

    Refactor codebase + organize files

---
+++ script.js
@@ -0,0 +1,6 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('test').addEventListener('click', function() {
+    window.location.href = 'squid/box.html';
+  });
+});
