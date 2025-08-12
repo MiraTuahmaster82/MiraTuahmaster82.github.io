2a1068f1f36ea3ba321fe73d04b55a911b49a726
Mira Donohue <Miradonohue@protonmail.com>
2025-08-12 15:08:52 -0500

    removal of girlfriend #single

--- script.js
+++
@@ -1,6 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('test').addEventListener('click', function() {
-    window.location.href = 'squid/box.html';
-  });
-});
